e129edbe73addbf376934b526ff54571bd42de0a
Joshua <joshua.richardson@myunidays.com>
2019-08-09 15:50:58 +0100

    Added initial files to the project

---
+++ MustardBlack.Example/MustardBlack.Example/Areas/Example/Example/ExampleAreaRegistration.cs
@@ -0,0 +1,12 @@
+﻿using MustardBlack.Areas;
+
+namespace MustardBlack.Example.Areas.Example
+{
+    sealed class ExampleAreaRegistration : AreaRegistrationBase
+    {
+        public ExampleAreaRegistration() : base(true)
+        {
+
+        }
+    }
+}
